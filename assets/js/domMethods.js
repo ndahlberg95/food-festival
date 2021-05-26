e7eae4a855a6fc7232c2ca7585f674462bf885b5
Nora Dahlberg <ndahlberg95@gmail.com>
2021-05-26 15:00:53 -0600

    optimize images

--- assets/js/domMethods.js
+++ assets/js/domMethods.js
@@ -1,38 +1,34 @@
-require("bootstrap");
-const createEl = require("./domMethods");
+function createEl(htmlString, attrs, ...children) {
+    if (typeof htmlString !== "string") {
+        throw Error("Argument 'htmlString' is required and must be a string");
+    }
 
-$(document).ready(function () {
-    function createEl(htmlString, attrs, ...children) {
-        if (typeof htmlString !== "string") {
-            throw Error("Argument 'htmlString' is required and must be a string");
-        }
+    const el = document.createElement(htmlString);
 
-        const el = document.createElement(htmlString);
-
-        if (typeof attrs === "object") {
-            for (let key in attrs) {
-                if (key.substring(0, 2) === "on") {
-                    el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
-                } else {
-                    el.setAttribute(key, attrs[key]);
-                }
-            }
-        }
-
-        children.forEach(function (child) {
-            let node;
-
-            if (child.constructor.name.includes("Element")) {
-                node = child;
+    if (typeof attrs === "object") {
+        for (let key in attrs) {
+            if (key.substring(0, 2) === "on") {
+                el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
             } else {
-                node = document.createTextNode(child);
+                el.setAttribute(key, attrs[key]);
             }
+        }
+    }
 
-            el.appendChild(node);
-        });
+    children.forEach(function (child) {
+        let node;
 
-        return el;
-    };
-})
+        if (child.constructor.name.includes("Element")) {
+            node = child;
+        } else {
+            node = document.createTextNode(child);
+        }
+
+        el.appendChild(node);
+    });
+
+    return el;
+};
+
 
 module.exports = createEl;
