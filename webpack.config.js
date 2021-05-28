c59385f9ac753670f37566323aaceaef2de8a411
Nora Dahlberg <ndahlberg95@gmail.com>
2021-05-28 17:01:15 -0600

    final update

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,10 @@
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
-const webpack = require("webpack");
 
 module.exports = {
-    entry: {
-        app: "./assets/js/script.js",
-        events: "./assets/js/events.js",
-        schedule: "./assets/js/schedule.js",
-        tickets: "./assets/js/tickets.js"
-    },
+    entry: './assets/js/script.js',
     output: {
-        filename: "[name].bundle.js",
-        path: __dirname + "/dist",
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
     },
-    module: {
-      rules: [
-        {
-          test: /\.jpg$/i,
-          use: [
-            {
-              loader: 'file-loader'
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery"
-        }),
-        new BundleAnalyzerPlugin({
-          analyzerMode: "static", // the report outputs to an HTML file in the dist folder
-        })
-    ],    
     mode: 'development'
 };
